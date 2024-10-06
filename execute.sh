26cd48c6d7e5a95d101d9d568e0d1c2c1f6d4dbf
Ananth <ananthp77@gmail.com>
2024-10-06 17:00:16 +0100

    changes

--- execute.sh
+++ execute.sh
@@ -9,19 +9,15 @@ fi
 # Capture the input file path
 INPUT_FILE=$1
 
-# Step 1: Navigate to the cloned directory
-echo "Navigating to df-file-job directory..."
-cd df-file-job || exit
-
-# Step 2: Build the project using Maven
+# Step 1: Build the project using Maven
 echo "Running Maven clean install..."
 mvn clean install
 
-# Step 3: Navigate to the target directory
+# Step 2: Navigate to the target directory
 echo "Navigating to target directory..."
 cd target || exit
 
-# Step 4: Execute the JAR file with the inputFile argument
+# Step 3: Execute the JAR file with the inputFile argument
 echo "Executing the Dataflow job with input file: $INPUT_FILE"
 java -jar dataflow-execute.jar --inputFile="$INPUT_FILE"
 
